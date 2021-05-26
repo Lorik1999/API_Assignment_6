faaf996dfc4741530d359b30c52fae24bcbaf24f
Lorik1999 <https://github.com/ovmo/titan.git>
2021-05-26 18:14:47 +0200

    Update post/request

--- API_server/models.py
+++ API_server/models.py
@@ -33,3 +33,4 @@ class Order(models.Model):
 
 
 
+
--- API_server/views.py
+++ API_server/views.py
@@ -6,8 +6,9 @@ from rest_framework.exceptions import NotFound
 from API_server.models import Pizza
 from API_server.models import Topping
 from django.core.exceptions import ObjectDoesNotExist
+from API_server.models import Order
 import json
-
+from datetime import datetime,timedelta
 # Create your views here.
 pizza_orders = {}
 
@@ -69,10 +70,46 @@ def get_pizza(request,pizza_id=-1):
 # TODO: use try and accept to catch if json.loads() failes, meaning incorrect format
 def order_pizza(request):
     if request.method=='POST':
-        data = request.body.decode('utf-8')
-        received_json_data = json.loads(data)
-        print(received_json_data['pizzas'])
-        return Response('Success')
+        try:
+            data = request.body.decode('utf-8')
+            received_json_data = json.loads(data)
+            print(received_json_data['pizzas'])
+            current_time= datetime.now()
+            time_diff= timedelta(days=0,hours=1,minutes=0)
+            delivery_time= current_time+time_diff
+            order=Order(takeaway=received_json_data['takeaway'],
+                        payment_type=received_json_data['payment_type'],
+                        customer_id=received_json_data['customer_id'],
+                        note=received_json_data['note'],
+                        street=received_json_data['delivery_address']['street'],
+                        city=received_json_data['delivery_address']['street'],
+                        country=received_json_data['delivery_address']['country'],
+                        zipcode=received_json_data['delivery_address']['zipcode'],
+                        ordered_at=datetime.now(),
+                        status='in progress',
+                        delivery_time=delivery_time
+                        )
+            order.save()
+            for id in received_json_data['pizzas']:
+                pizza_obj= Pizza.objects.get(pizza_id=id)
+                order.pizzas.add(pizza_obj)
+            order.save()
+
+
+            return_data= {}
+            return_data['order']={}
+            return_data['order']['order_id']=order.id
+            return_data['order']['ordered_at'] = order.ordered_at
+            return_data['order']['status']=order.status
+            return_data['delivery_time']= order.delivery_time
+
+            return Response(return_data)
+        except Exception as e:
+            print(e)
+            errormessage= {
+                'message':'The format of the object is invalid'
+            }
+            raise NotFound(detail=errormessage,code=400)
     return Response('Failed')
 
 
