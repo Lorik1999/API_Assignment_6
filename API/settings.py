9e7fc8899cab6f4b95ed43924de02a77bfbe2040
Lorik1999 <https://github.com/ovmo/titan.git>
2021-05-28 20:19:41 +0200

    update settings.py

--- API/settings.py
+++ API/settings.py
@@ -133,12 +133,13 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'API_server/static'),
-
-    os.path.join(BASE_DIR, 'boot'),
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'API_server/static'),
+#
+#     os.path.join(BASE_DIR, 'boot'),
+# ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
